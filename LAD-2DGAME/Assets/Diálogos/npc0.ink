a81fe9946f4578a86094a7ee029b25af83b2738b
David García Valero <davidgarciavalero@gmail.com>
2025-06-16 13:40:39 +0200

    Tweak 1 Dialogos basicos

--- LAD-2DGAME/Assets/Diálogos/npc0.ink
+++ LAD-2DGAME/Assets/Diálogos/npc0.ink
@@ -1,3 +1,11 @@
+=== Escena1 ===
+
+= Dialogo1
 ¡Esto es una prueba!
-¿Has traido las patatas fritas?
+¿Has traído las patatas fritas, no?
 ¡Son imprescindibles para el rol!
+-> END
+
+= Dialogo2
+¡Coge asiento!
+-> END
--- LAD-2DGAME/Assets/Script/Diálogos/DialogueManager.cs
+++ LAD-2DGAME/Assets/Script/Diálogos/DialogueManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1523e7952f35cb88ac545031816348fbe394bda841b15bbe1548674b60ef688
-size 2012
+oid sha256:ec92a76a906f2eed38ec67cb8ec1eaaafe7fb0a9c0951d038cdf0315592c4ece
+size 2117
