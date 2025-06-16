c820c8a2b00597e2a3beea9a79369af3e90008cc
David García Valero <davidgarciavalero@gmail.com>
2025-06-16 19:11:18 +0200

    Revert "Merge branch 'Potato1' into David-Pruebas"
    This reverts commit f4338e44695d9f9faefe0991cb4e09ce3cc61081, reversing
    changes made to e828b86f0763eb37f94a08e49d42b7307e16e869.

--- LAD-2DGAME/Assets/Script/Attack.cs
+++ LAD-2DGAME/Assets/Script/Attack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e14221b065e953c20caa61e7d94338b9a4e20cfb63cf1edd85a9fba6634401d
-size 258
+oid sha256:2650b5a0a1114a8b459b60d2660af8f59cf2c1d61c5646240a756852fd399e73
+size 962
