3ca68a075ed0aeda2831762b858af7ea4a0b73e3
David García Valero <davidgarciavalero@gmail.com>
2025-06-16 18:56:40 +0200

    arreglar merge player movement

--- LAD-2DGAME/Assets/Script/PlayerController.cs
+++ LAD-2DGAME/Assets/Script/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aebdd54329822023806292d61b2a5cc293e8db7799ca99d1a201331e7451efcd
-size 1226
+oid sha256:a0bef875c2649c1abde64b6e1b08962287ec619f8a4374dbb3ef7cd8da70a9a9
+size 1031
---
+++ LAD-2DGAME/Assets/Script/Player_Movement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a55aa76c24ab329dd24282cd7770785c818b4ae056a1d544a132e1887222656
+size 1441
