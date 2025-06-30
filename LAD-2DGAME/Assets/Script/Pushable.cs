4233d82f95c2d95a55d3c29db34b71d4269c3ae6
AbelSeMo <serranomonabel@gmail.com>
2025-06-30 15:27:07 +0200

    Save

--- LAD-2DGAME/Assets/Script/Pushable.cs
+++ LAD-2DGAME/Assets/Script/Pushable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bd3a74880919e4efaac583e7b33a2b6fd698a8c1b2899068ad0e5ca62fee306
-size 265
+oid sha256:f77c8073a744c7c15429eeb64ee4562d2e014ef190c60ec82a3586f6d30d9c72
+size 1590
